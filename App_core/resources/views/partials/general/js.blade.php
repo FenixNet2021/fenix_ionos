b63170d8a19823554d6cf66329511e1639c51b07
Gam Vel <gam03aliel@gmail.com>
2024-08-26 14:06:53 -0600

    Subida de proyecto

---
+++ App_core/resources/views/partials/general/js.blade.php
@@ -0,0 +1,7 @@
+<script src="{{URL::asset('assets/bundles/libscripts.bundle.js')}}"></script>
+<script src="{{URL::asset('assets/bundles/vendorscript.bundle.js')}}"></script>
+
+<script src="{{URL::asset('assets/bundles/mainscripts.bundle.js')}}"></script>
+
+{{--
+<script src="{{URL::asset('assets/plugins/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js')}}"></script>--}}
